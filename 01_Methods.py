4c51affa20c2c0bab962d33b1dbcff3d21f801d3
UDAY RAMA KOTESWA RAO NALABOTHU <111573702+uadaynalabothu@users.noreply.github.com>
2022-09-09 16:22:42 +0530

    Add files via upload

---
+++ 01_Methods.py
@@ -0,0 +1,130 @@
+# print(dir(int)) 
+
+# print(dir(float))
+
+# print(dir(str))
+
+# string= ['capitalize', 'casefold', 'center', 'count', 'encode', 'endswith', 'expandtabs', 
+# 'find', 'format', 'format_map', 'index', 'isalnum', 'isalpha', 'isascii', 'isdecimal', 
+# 'isdigit', 'isidentifier', 'islower', 'isnumeric', 'isprintable', 'isspace', 'istitle', 
+# 'isupper', 'join', 'ljust', 'lower', 'lstrip', 'maketrans', 'partition', 'removeprefix', 
+# 'removesuffix', 'replace', 'rfind', 'rindex', 'rjust', 'rpartition', 'rsplit', 'rstrip', 
+# 'split', 'splitlines', 'startswith', 'strip', 'swapcase', 'title', 'translate', 'upper', 'zfill' ]
+
+# # print(dir(bool))
+
+# print(dir(None))
+
+# print(dir(list))
+
+#  list=['append', 'clear', 'copy', 'count', 'extend', 'index', 
+#       'insert', 'pop', 'remove', 'reverse', 'sort'] 
+
+# print(dir(tuple))
+
+   # """ 'count', 'index' """
+
+# print(dir(set))
+
+# """ 'add', 'clear', 'copy', 'difference', 'difference_update', 'discard', 
+#    'intersection', 'intersection_update', 'isdisjoint', 'issubset', 'issuperset',
+#      'pop', 'remove', 'symmetric_difference', 'symmetric_difference_update', 
+#       'union', 'update'   """
+
+
+# print(dir(dict))
+
+# """ 'clear', 'copy', 'fromkeys', 'get', 'items', 'keys', 'pop', 'popitem', 
+#         'setdefault', 'update', 'values'  """
+
+# print(dir(bytes))
+
+# """ 'capitalize', 'center', 'count', 'decode', 'endswith', 'expandtabs', 
+# 'find', 'fromhex', 'hex', 'index', 'isalnum', 'isalpha', 'isascii', 'isdigit', 
+# 'islower', 'isspace', 'istitle', 'isupper', 'join', 'ljust', 'lower', 'lstrip', 
+# 'maketrans', 'partition', 'removeprefix', 'removesuffix', 'replace', 'rfind', 'rindex', 
+# 'rjust', 'rpartition', 'rsplit', 'rstrip', 'split', 'splitlines', 'startswith', 'strip', 
+#         'swapcase', 'title', 'translate', 'upper', 'zfill'   """
+
+
+# print(dir(complex))
+
+# """ 'conjugate', 'imag', 'real'  """
+
+
+# 1. capitalize 
+# 2. casefold
+# 3. center
+# 4. count
+# 5. encode
+# 6. endwith
+# 7. expandtabs
+# 8. find
+# 9. format
+# 10. index
+# 11. isalnum
+# 12. isascii
+# 13. isdecimal
+# 14. isdigital
+# 15. isidentifier
+# 16. islower
+# 17. isnumeric
+# 18. isprintable
+# 19. isspace
+# 20. istitle
+# 21. isupper
+# 22. join
+# 23. ljust
+# 24. lower
+# 25. upper
+# 26. lstrip
+# 27. maketrans
+# 28. partition
+# 29. removeprefix
+# 30. removesuffix
+# 31. replace
+# 32. rfind
+# 33. rindex
+# 34. rjust
+# 35. rsplit
+# 36. rstrip
+# 37. rpartition
+# 38. split
+# 39. splitlines
+# 40. startswith
+# 41. swapcase
+# 42. strip
+# 43. title
+# 44. translate
+# 45. zfill
+
+
+# 46. append
+# 47. clear
+# 48. copy
+# 49. extend
+# 50. insert
+# 51. pop
+# 52. remove
+# 53. reverse
+# 54. sort
+
+
+# 55. add
+# 56. difference 
+# 57. update
+# 58. union
+# 59. intersection
+# 60. discard
+# 61. isdisjoint
+# 62. issubset
+# 63. issuperset
+
+
+# 64. popitem
+# 65. fromkeys
+# 66. get
+# 67. items
+# 68. keys
+# 69. values
+# 70. setdefault
