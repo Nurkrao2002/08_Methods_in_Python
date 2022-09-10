edd536e2d396f5bd0ba2d208153c774c9192fa97
UDAY RAMA KOTESWA RAO NALABOTHU <111573702+uadaynalabothu@users.noreply.github.com>
2022-09-10 23:01:26 +0530

    Update 01_Methods.py

--- 01_Methods.py
+++ 01_Methods.py
@@ -1,130 +1,156 @@
-# print(dir(int)) 
+print(dir(int)) 
 
-# print(dir(float))
+print(dir(float))
 
-# print(dir(str))
+print(dir(str))
 
-# string= ['capitalize', 'casefold', 'center', 'count', 'encode', 'endswith', 'expandtabs', 
-# 'find', 'format', 'format_map', 'index', 'isalnum', 'isalpha', 'isascii', 'isdecimal', 
-# 'isdigit', 'isidentifier', 'islower', 'isnumeric', 'isprintable', 'isspace', 'istitle', 
-# 'isupper', 'join', 'ljust', 'lower', 'lstrip', 'maketrans', 'partition', 'removeprefix', 
-# 'removesuffix', 'replace', 'rfind', 'rindex', 'rjust', 'rpartition', 'rsplit', 'rstrip', 
-# 'split', 'splitlines', 'startswith', 'strip', 'swapcase', 'title', 'translate', 'upper', 'zfill' ]
+print(len('capitalize', 'casefold', 'center', 'count', 'encode', 'endswith', 'expandtabs', 
+'find', 'format', 'format_map', 'index', 'isalnum', 'isalpha', 'isascii', 'isdecimal', 
+'isdigit', 'isidentifier', 'islower', 'isnumeric', 'isprintable', 'isspace', 'istitle', 
+'isupper', 'join', 'ljust', 'lower', 'lstrip', 'maketrans', 'partition', 'removeprefix', 
+'removesuffix', 'replace', 'rfind', 'rindex', 'rjust', 'rpartition', 'rsplit', 'rstrip', 
+'split', 'splitlines', 'startswith', 'strip', 'swapcase', 'title', 'translate', 'upper', 'zfill')
 
-# # print(dir(bool))
+print(dir(bool))
 
-# print(dir(None))
+print(dir(None))
 
-# print(dir(list))
+print(dir(list))
 
-#  list=['append', 'clear', 'copy', 'count', 'extend', 'index', 
-#       'insert', 'pop', 'remove', 'reverse', 'sort'] 
+ list=['append', 'clear', 'copy', 'count', 'extend', 'index', 
+      'insert', 'pop', 'remove', 'reverse', 'sort'] 
 
-# print(dir(tuple))
+print(dir(tuple))
 
-   # """ 'count', 'index' """
+   """ 'count', 'index' """
 
-# print(dir(set))
+print(dir(set))
 
-# """ 'add', 'clear', 'copy', 'difference', 'difference_update', 'discard', 
-#    'intersection', 'intersection_update', 'isdisjoint', 'issubset', 'issuperset',
-#      'pop', 'remove', 'symmetric_difference', 'symmetric_difference_update', 
-#       'union', 'update'   """
-
-
-# print(dir(dict))
-
-# """ 'clear', 'copy', 'fromkeys', 'get', 'items', 'keys', 'pop', 'popitem', 
-#         'setdefault', 'update', 'values'  """
-
-# print(dir(bytes))
-
-# """ 'capitalize', 'center', 'count', 'decode', 'endswith', 'expandtabs', 
-# 'find', 'fromhex', 'hex', 'index', 'isalnum', 'isalpha', 'isascii', 'isdigit', 
-# 'islower', 'isspace', 'istitle', 'isupper', 'join', 'ljust', 'lower', 'lstrip', 
-# 'maketrans', 'partition', 'removeprefix', 'removesuffix', 'replace', 'rfind', 'rindex', 
-# 'rjust', 'rpartition', 'rsplit', 'rstrip', 'split', 'splitlines', 'startswith', 'strip', 
-#         'swapcase', 'title', 'translate', 'upper', 'zfill'   """
+""" 'add', 'clear', 'copy', 'difference', 'difference_update', 'discard', 
+   'intersection', 'intersection_update', 'isdisjoint', 'issubset', 'issuperset',
+     'pop', 'remove', 'symmetric_difference', 'symmetric_difference_update', 
+      'union', 'update'   """
 
 
-# print(dir(complex))
+print(dir(dict))
 
-# """ 'conjugate', 'imag', 'real'  """
+""" 'clear', 'copy', 'fromkeys', 'get', 'items', 'keys', 'pop', 'popitem', 
+        'setdefault', 'update', 'values'  """
 
+print(dir(bytes))
 
-# 1. capitalize 
-# 2. casefold
-# 3. center
-# 4. count
-# 5. encode
-# 6. endwith
-# 7. expandtabs
-# 8. find
-# 9. format
-# 10. index
-# 11. isalnum
-# 12. isascii
-# 13. isdecimal
-# 14. isdigital
-# 15. isidentifier
-# 16. islower
-# 17. isnumeric
-# 18. isprintable
-# 19. isspace
-# 20. istitle
-# 21. isupper
-# 22. join
-# 23. ljust
-# 24. lower
-# 25. upper
-# 26. lstrip
-# 27. maketrans
-# 28. partition
-# 29. removeprefix
-# 30. removesuffix
-# 31. replace
-# 32. rfind
-# 33. rindex
-# 34. rjust
-# 35. rsplit
-# 36. rstrip
-# 37. rpartition
-# 38. split
-# 39. splitlines
-# 40. startswith
-# 41. swapcase
-# 42. strip
-# 43. title
-# 44. translate
-# 45. zfill
+ print(len('capitalize', 'center', 'count', 'decode', 'endswith', 'expandtabs', 
+'find', 'fromhex', 'hex', 'index', 'isalnum', 'isalpha', 'isascii', 'isdigit', 
+'islower', 'isspace', 'istitle', 'isupper', 'join', 'ljust', 'lower', 'lstrip', 
+'maketrans', 'partition', 'removeprefix', 'removesuffix', 'replace', 'rfind', 'rindex', 
+'rjust', 'rpartition', 'rsplit', 'rstrip', 'split', 'splitlines', 'startswith', 'strip', 
+        'swapcase', 'title', 'translate', 'upper', 'zfill') 
 
 
-# 46. append
-# 47. clear
-# 48. copy
-# 49. extend
-# 50. insert
-# 51. pop
-# 52. remove
-# 53. reverse
-# 54. sort
+print(dir(complex))
+
+""" 'conjugate', 'imag', 'real'  """
 
 
-# 55. add
-# 56. difference 
-# 57. update
-# 58. union
-# 59. intersection
-# 60. discard
-# 61. isdisjoint
-# 62. issubset
-# 63. issuperset
+print(dir(pandas))
+
+print(len(['BooleanDtype', 'Categorical', 'CategoricalDtype', 'CategoricalIndex', 'DataFrame',
+'DateOffset', 'DatetimeIndex', 'DatetimeTZDtype', 'ExcelFile', 'ExcelWriter', 'Flags', 'Float32Dtype', 
+'Float64Dtype', 'Float64Index', 'Grouper', 'HDFStore', 'Index', 'IndexSlice', 'Int16Dtype', 'Int32Dtype',
+'Int64Dtype', 'Int64Index', 'Int8Dtype', 'Interval', 'IntervalDtype', 'IntervalIndex', 'MultiIndex',
+'NA', 'NaT', 'NamedAgg', 'Period', 'PeriodDtype', 'PeriodIndex', 'RangeIndex', 'Series', 'SparseDtype',
+'StringDtype', 'Timedelta', 'TimedeltaIndex', 'Timestamp', 'UInt16Dtype', 'UInt32Dtype', 'UInt64Dtype',
+'UInt64Index', 'UInt8Dtype', '__builtins__', '__cached__', '__doc__', '__docformat__', '__file__',
+'__getattr__', '__git_version__', '__loader__', '__name__', '__package__', '__path__', '__spec__',
+'__version__', '_config', '_hashtable', '_is_numpy_dev', '_lib', '_libs', '_np_version_under1p18',
+'_testing', '_tslib', '_typing', '_version', 'api', 'array', 'arrays', 'bdate_range', 'compat',
+'concat', 'core', 'crosstab', 'cut', 'date_range', 'describe_option', 'errors', 'eval', 'factorize',
+'get_dummies', 'get_option', 'infer_freq', 'interval_range', 'io', 'isna', 'isnull','json_normalize', 
+'lreshape', 'melt', 'merge', 'merge_asof', 'merge_ordered', 'notna', 'notnull','offsets', 'option_context',
+'options', 'pandas', 'period_range', 'pivot', 'pivot_table', 'plotting', 'qcut', 'read_clipboard',
+'read_csv', 'read_excel', 'read_feather', 'read_fwf', 'read_gbq', 'read_hdf', 'read_html', 'read_json',
+'read_orc', 'read_parquet', 'read_pickle', 'read_sas', 'read_spss', 'read_sql','read_sql_query',
+'read_sql_table', 'read_stata', 'read_table', 'read_xml', 'reset_option', 'set_eng_float_format', 
+'set_option', 'show_versions', 'test', 'testing', 'timedelta_range', 'to_datetime', 'to_numeric', 
+'to_pickle', 'to_timedelta', 'tseries', 'unique', 'util', 'value_counts','wide_to_long']))
 
 
-# 64. popitem
-# 65. fromkeys
-# 66. get
-# 67. items
-# 68. keys
-# 69. values
-# 70. setdefault
+1. capitalize 
+2. casefold
+3. center
+4. count
+5. encode
+6. endwith
+7. expandtabs
+8. find
+9. format
+10. index
+11. isalnum
+12. isascii
+13. isdecimal
+14. isdigital
+15. isidentifier
+16. islower
+17. isnumeric
+18. isprintable
+19. isspace
+20. istitle
+21. isupper
+22. join
+23. ljust
+24. lower
+25. upper
+26. lstrip
+27. maketrans
+28. partition
+29. removeprefix
+30. removesuffix
+31. replace
+32. rfind
+33. rindex
+34. rjust
+35. rsplit
+36. rstrip
+37. rpartition
+38. split
+39. splitlines
+40. startswith
+41. swapcase
+42. strip
+43. title
+44. translate
+45. zfill
+
+
+46. append
+47. clear
+48. copy
+49. extend
+50. insert
+51. pop
+52. remove
+53. reverse
+54. sort
+
+
+55. add
+56. difference 
+57. update
+58. union
+59. intersection
+60. discard
+61. isdisjoint
+62. issubset
+63. issuperset
+
+
+64. popitem
+65. fromkeys
+66. get
+67. items
+68. keys
+69. values
+70. setdefault
+       
+       
+       
